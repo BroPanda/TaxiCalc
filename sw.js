c8da55247d67068f086ad99cacdda379d7e9549c
BroPanda <BroPanda@users.noreply.github.com>
2026-05-01 01:13:43 +0300

    Add PWA requirements for APK

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'taxicalc-v1';
+const ASSETS = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json',
+    './icon.svg',
+    './icon-192.png',
+    './icon-512.png',
+    './screenshot1.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
